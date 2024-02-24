a1372d2b46e22c213c6f2ce6a9512ce1cfefa694
ratneshkumarprajapati <ratneshkumarprajapati367@gmail.com>
2024-02-24 19:19:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//map filter
+
+//without map
+
+const inputArray=[1,2,3,4,5]
+
+//multiply each element by 2
+const newArray=[];
+for (let i = 0; i < inputArray.length; i++) {
+    newArray.push(inputArray[i]*2)
+    
+}
+console.log(newArray)
